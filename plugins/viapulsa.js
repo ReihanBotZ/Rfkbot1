9869b984b366b76dfdbb2fa4c436bef1bbd8dca4
ReihanBotZ <100804584+ReihanBotZ@users.noreply.github.com>
2022-03-31 13:01:00 +0700

    Update viapulsa.js

--- plugins/viapulsa.js
+++ plugins/viapulsa.js
@@ -2,7 +2,7 @@ let handler = async (m, { conn }) => {
 	conn.reply(m.chat, `╠═〘 PEMBAYARAN 〙 ═
 ╠═ Untuk Pembayaran Via Pulsa
 ╠➥ Ke nomer ini
-║➥ 085730903853
+║➥ 085368159958
 ║
 ╠═〘 Fitur 〙 ═
 ║
@@ -22,8 +22,8 @@ let handler = async (m, { conn }) => {
 ╠➥ *DAN 200 LEBIH*
 ║   *FITUR LAINNYA*
 ║ 
-╠═ ©2021 Rpg wabot-aq
-╠═ Scrip original by Nurutomo
+╠═ ©2022 ReihanBotZ
+╠═ Scrip original by Muhammad Yesar✓
 ╠═〘 ${namabot} 〙 ═`.trim(), m)
 }
 
